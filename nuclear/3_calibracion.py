5a1f61f6f892707106d9f47d65854864aa5510f4
deivis <david.cotelo.online+github@gmail.com>
2026-04-09 19:20:25 +0200

    Media calibración feita
    Depende de https://redirect.github.com/dcoutogarcia/mca-tools/pull/16
    
    Non ten en conta as enerxías, pero bueh

--- nuclear/3_calibracion.py
+++ nuclear/3_calibracion.py
@@ -1,11 +1,36 @@
 import mca_tools as mca
 mca.select_language("gl")
 
-mostra_bi207 = mca.peakSelector("datos/calibracion/mostra_bi207.mca", bkg_file = "datos/background.mca", fig_path = "memoria/figuras/")
-mostra_co60  = mca.peakSelector("datos/calibracion/mostra_co60.mca", bkg_file = "datos/background.mca", fig_path = "memoria/figuras/")
-mostra_cs137 = mca.peakSelector("datos/calibracion/mostra_cs137.mca", bkg_file = "datos/background.mca", fig_path = "memoria/figuras/")
-mostra_eu152 = mca.peakSelector("datos/calibracion/mostra_eu152.mca", bkg_file = "datos/background.mca", fig_path = "memoria/figuras/")
-mostra_na22  = mca.peakSelector("datos/calibracion/mostra_na22.mca", bkg_file = "datos/background.mca", fig_path = "memoria/figuras/")
+mostra_bi207 = mca.peakSelector(
+    "datos/calibracion/mostra_bi207.mca",
+    bkg_file = "datos/background.mca",
+    fig_path = "memoria/figuras/",
+    csv_path = "memoria/datos/"
+)
+mostra_co60  = mca.peakSelector(
+    "datos/calibracion/mostra_co60.mca",
+    bkg_file = "datos/background.mca",
+    fig_path = "memoria/figuras/",
+    csv_path = "memoria/datos/"
+)
+mostra_cs137 = mca.peakSelector(
+    "datos/calibracion/mostra_cs137.mca",
+    bkg_file = "datos/background.mca",
+    fig_path = "memoria/figuras/",
+    csv_path = "memoria/datos/"
+)
+mostra_eu152 = mca.peakSelector(
+    "datos/calibracion/mostra_eu152.mca",
+    bkg_file = "datos/background.mca",
+    fig_path = "memoria/figuras/",
+    csv_path = "memoria/datos/"
+)
+mostra_na22  = mca.peakSelector(
+    "datos/calibracion/mostra_na22.mca",
+    bkg_file = "datos/background.mca",
+    fig_path = "memoria/figuras/",
+    csv_path = "memoria/datos/"
+)
 
 print("Datos Cargados")
 
@@ -16,3 +41,24 @@ mostra_eu152.plot()
 mostra_na22.plot()
 
 print("Gráficas feitas")
+print("Pasamos ao fitting")
+
+mostra_bi207.select_peaks()
+mostra_co60.select_peaks()
+mostra_cs137.select_peaks()
+mostra_eu152.select_peaks()
+mostra_na22.select_peaks()
+
+print("Picos axustados")
+
+# print("Pasamos a calibración")
+#
+# mca.calibration([
+#     mostra_bi207,
+#     mostra_co60,
+#     mostra_cs137,
+#     mostra_eu152,
+#     mostra_na22,
+# ])
+#
+# print("Calibración feita")
--- nuclear/memoria/seccions/3_01_RESULTADOS_calibracion.typ
+++ nuclear/memoria/seccions/3_01_RESULTADOS_calibracion.typ
@@ -1,1 +1,36 @@
 == Calibración
+
+Móstranse os axustes a diversos picos das mostras que usamos para a calibración.
+
+#let fig_cal = {
+    sys.inputs.at("figuras_fits_calibracion")
+        .split(" ")
+        .map(x => x.trim("memoria"))
+}
+
+#for i in range(fig_cal.len()) {
+    figure(
+        image( fig_cal.at(i) ),
+        caption: fig_cal.at(i).trim("/figuras/").trim(".pdf")
+    )
+}
+
+#let csv_cal = {
+    sys.inputs.at("csv_calibracion")
+        .split(" ")
+        .map(x => x.trim("memoria"))
+}
+
+
+#block(
+    width: 200%,
+    for i in range(csv_cal.len()) {
+        set text(size: 0.8em)
+        table(
+            columns: 8,
+            ..csv( csv_cal.at(i) ).at(0).map(x => eval(x)),
+            ..csv( csv_cal.at(i) ).slice(1).flatten(),
+
+        )
+    }
+)
