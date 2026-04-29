3240e353ca5d389c8525ac8bc368dd7a04ea76ab
deivis <david.cotelo.online+github@gmail.com>
2026-04-29 17:20:36 +0200

    Rematar todos os axuste

--- solido/0_analise.py
+++ solido/0_analise.py
@@ -10,13 +10,6 @@ def equivalente(theta, a, b, m):
     coseno = np.cos(theta)**2
     bcoseno = b * coseno
     a_bcoseno = a + bcoseno
-    # print(f"theta: {theta}")
-    # print(f"a: {a}")
-    # print(f"b: {b}")
-    # print(f"m: {m}")
-    # print(f"coseno: {coseno}")
-    # print(f"b * coseno: {bcoseno}")
-    # print(f"a + b * coseno: {a_bcoseno}")
     return np.sqrt(abs(a_bcoseno)**m)
 
 def independente(theta, a, b, m):
@@ -78,7 +71,7 @@ axuste_fluxo_ambiental_total_eq = sco.curve_fit(
     A_fluxo_ambiental_total,
     G_fluxo_ambiental_total,
     # sigma = [0.000001] * len(A_fluxo_ambiental_total),
-    p0 = (2, 0.00000001, 1.5,), # a, b , m
+    p0 = (0.00000001, 0.00000001, 1.5,), # a, b , m
 )
 
 axuste_fluxo_ambiental_total_in = sco.curve_fit(
@@ -112,7 +105,122 @@ x = np.linspace(A_fluxo_ambiental_total[0], A_fluxo_ambiental_total[-1], 100)
 y_eq = equivalente(x, a1_optimizado_eq, b1_optimizado_eq, m1_optimizado_eq)
 y_in = independente(x, a1_optimizado_in, b1_optimizado_in, m1_optimizado_in)
 lenzo_2.plot(x, y_eq, linestyle="dashed", marker="", color="green" , label="Equivalente")
-lenzo_2.plot(x, y_in, linestyle="dashed", marker="", color="orange", label="Independente")
+lenzo_2.plot(x, y_in, linestyle="dashdot", marker="", color="purple", label="Independente")
 lenzo_2.plot(A_fluxo_ambiental_total, G_fluxo_ambiental_total, color="red", label = "Datos")
 figura_2.legend()
 figura_2.savefig("memoria/figuras/axuste_fluxo_total.pdf")
+
+
+##############################################################################
+# FLUXO AMBIENTAL PARCIAL ####################################################
+##############################################################################
+
+datos_fluxo_ambiental_parcial = np.loadtxt("datos/fluxo_ambiental_parcial.dat", delimiter = ',')
+
+parte = slice(0, len(datos_fluxo_ambiental_parcial[0]))
+I_fluxo_ambiental_parcial = datos_fluxo_ambiental_parcial[0][parte] * 10 ** (-6)
+A_fluxo_ambiental_parcial = datos_fluxo_ambiental_parcial[1][parte] * np.pi / 180
+V_fluxo_ambiental_parcial = datos_fluxo_ambiental_parcial[2][parte]
+
+G_fluxo_ambiental_parcial = I_fluxo_ambiental_parcial / V_fluxo_ambiental_parcial
+
+axuste_fluxo_ambiental_parcial_eq = sco.curve_fit(
+    equivalente,
+    A_fluxo_ambiental_parcial,
+    G_fluxo_ambiental_parcial,
+    # sigma = [0.000001] * len(A_fluxo_ambiental_parcial),
+    p0 = (0.00000001, 0.00000001, 1.5,), # a, b , m
+)
+
+axuste_fluxo_ambiental_parcial_in = sco.curve_fit(
+    independente,
+    A_fluxo_ambiental_parcial,
+    G_fluxo_ambiental_parcial,
+    # sigma = [0.000001] * len(A_fluxo_ambiental_parcial),
+    p0 = (2, 0.00000001, 1.5,), # a, b , m
+)
+
+a1_optimizado_eq = axuste_fluxo_ambiental_parcial_eq[0][0]
+b1_optimizado_eq = axuste_fluxo_ambiental_parcial_eq[0][1]
+m1_optimizado_eq = axuste_fluxo_ambiental_parcial_eq[0][2]
+
+a1_optimizado_in = axuste_fluxo_ambiental_parcial_in[0][0]
+b1_optimizado_in = axuste_fluxo_ambiental_parcial_in[0][1]
+m1_optimizado_in = axuste_fluxo_ambiental_parcial_in[0][2]
+
+gd.garda_dato("a_fluxo_parcial_eq", f"{a1_optimizado_eq:.3g}", 0, "", "/memoria/datos.csv")
+gd.garda_dato("b_fluxo_parcial_eq", f"{b1_optimizado_eq:.3g}", 0, "", "/memoria/datos.csv")
+gd.garda_dato("m_fluxo_parcial_eq", f"{m1_optimizado_eq:.3g}", 0, "", "/memoria/datos.csv")
+gd.garda_dato("a_fluxo_parcial_in", f"{a1_optimizado_in:.3g}", 0, "", "/memoria/datos.csv")
+gd.garda_dato("b_fluxo_parcial_in", f"{b1_optimizado_in:.3g}", 0, "", "/memoria/datos.csv")
+gd.garda_dato("m_fluxo_parcial_in", f"{m1_optimizado_in:.3g}", 0, "", "/memoria/datos.csv")
+
+figura_2 = plt.figure()
+lenzo_2 = figura_2.add_subplot(1,1,1)
+lenzo_2.set(xlabel=r"$\theta$ [rad]", ylabel="$G$ [V / A]")
+
+x = np.linspace(A_fluxo_ambiental_parcial[0], A_fluxo_ambiental_parcial[-1], 100)
+y_eq = equivalente(x, a1_optimizado_eq, b1_optimizado_eq, m1_optimizado_eq)
+y_in = independente(x, a1_optimizado_in, b1_optimizado_in, m1_optimizado_in)
+lenzo_2.plot(x, y_eq, linestyle="dashed", marker="", color="green" , label="Equivalente")
+lenzo_2.plot(x, y_in, linestyle="dashdot", marker="", color="purple", label="Independente")
+lenzo_2.plot(A_fluxo_ambiental_parcial, G_fluxo_ambiental_parcial, color="red", label = "Datos")
+figura_2.legend()
+figura_2.savefig("memoria/figuras/axuste_fluxo_parcial.pdf")
+
+##############################################################################
+# FLUXO AMBIENTAL NULO #######################################################
+##############################################################################
+
+datos_fluxo_ambiental_nulo = np.loadtxt("datos/fluxo_ambiental_nulo.dat", delimiter = ',')
+
+parte = slice(0, len(datos_fluxo_ambiental_nulo[0]))
+I_fluxo_ambiental_nulo = datos_fluxo_ambiental_nulo[0][parte] * 10 ** (-6)
+A_fluxo_ambiental_nulo = datos_fluxo_ambiental_nulo[1][parte] * np.pi / 180
+V_fluxo_ambiental_nulo = datos_fluxo_ambiental_nulo[2][parte]
+
+G_fluxo_ambiental_nulo = I_fluxo_ambiental_nulo / V_fluxo_ambiental_nulo
+
+axuste_fluxo_ambiental_nulo_eq = sco.curve_fit(
+    equivalente,
+    A_fluxo_ambiental_nulo,
+    G_fluxo_ambiental_nulo,
+    # sigma = [0.000001] * len(A_fluxo_ambiental_nulo),
+    p0 = (0.00000001, 0.00000001, 1.5,), # a, b , m
+)
+
+axuste_fluxo_ambiental_nulo_in = sco.curve_fit(
+    independente,
+    A_fluxo_ambiental_nulo,
+    G_fluxo_ambiental_nulo,
+    # sigma = [0.000001] * len(A_fluxo_ambiental_nulo),
+    p0 = (2, 0.00000001, 1.5,), # a, b , m
+)
+
+a1_optimizado_eq = axuste_fluxo_ambiental_nulo_eq[0][0]
+b1_optimizado_eq = axuste_fluxo_ambiental_nulo_eq[0][1]
+m1_optimizado_eq = axuste_fluxo_ambiental_nulo_eq[0][2]
+
+a1_optimizado_in = axuste_fluxo_ambiental_nulo_in[0][0]
+b1_optimizado_in = axuste_fluxo_ambiental_nulo_in[0][1]
+m1_optimizado_in = axuste_fluxo_ambiental_nulo_in[0][2]
+
+gd.garda_dato("a_fluxo_nulo_eq", f"{a1_optimizado_eq:.3g}", 0, "", "/memoria/datos.csv")
+gd.garda_dato("b_fluxo_nulo_eq", f"{b1_optimizado_eq:.3g}", 0, "", "/memoria/datos.csv")
+gd.garda_dato("m_fluxo_nulo_eq", f"{m1_optimizado_eq:.3g}", 0, "", "/memoria/datos.csv")
+gd.garda_dato("a_fluxo_nulo_in", f"{a1_optimizado_in:.3g}", 0, "", "/memoria/datos.csv")
+gd.garda_dato("b_fluxo_nulo_in", f"{b1_optimizado_in:.3g}", 0, "", "/memoria/datos.csv")
+gd.garda_dato("m_fluxo_nulo_in", f"{m1_optimizado_in:.3g}", 0, "", "/memoria/datos.csv")
+
+figura_2 = plt.figure()
+lenzo_2 = figura_2.add_subplot(1,1,1)
+lenzo_2.set(xlabel=r"$\theta$ [rad]", ylabel="$G$ [V / A]")
+
+x = np.linspace(A_fluxo_ambiental_nulo[0], A_fluxo_ambiental_nulo[-1], 100)
+y_eq = equivalente(x, a1_optimizado_eq, b1_optimizado_eq, m1_optimizado_eq)
+y_in = independente(x, a1_optimizado_in, b1_optimizado_in, m1_optimizado_in)
+lenzo_2.plot(x, y_eq, linestyle="dashed", marker="", color="green" , label="Equivalente")
+lenzo_2.plot(x, y_in, linestyle="dashdot", marker="", color="purple", label="Independente")
+lenzo_2.plot(A_fluxo_ambiental_nulo, G_fluxo_ambiental_nulo, color="red", label = "Datos")
+figura_2.legend()
+figura_2.savefig("memoria/figuras/axuste_fluxo_nulo.pdf")
--- solido/memoria/seccions/3_00_RESULTADOS_introducion.typ
+++ solido/memoria/seccions/3_00_RESULTADOS_introducion.typ
@@ -46,3 +46,43 @@ Agora podemos realizar os axustes ás ecuacións @ec:equivalente @ec:independent
     image("/figuras/axuste_fluxo_total.pdf"),
     caption : [Axuste no caso de fluxo total.]
 ) <fig:axuste-total>
+
+#{
+    set align(center)
+    set text(size:0.9em)
+    table(
+        columns: 4,
+        stroke: none,
+        table.hline(),
+        table.header([Modelo], [a], [b], [m]),
+        table.hline(stroke: 0.6pt),
+        [Equivalente], valor("a_fluxo_parcial_eq"), valor("b_fluxo_parcial_eq"), valor("m_fluxo_parcial_eq"),
+        [Independente], valor("a_fluxo_parcial_in"), valor("b_fluxo_parcial_in"), valor("m_fluxo_parcial_in"),
+        table.hline(),
+    )
+}
+
+#figure(
+    image("/figuras/axuste_fluxo_parcial.pdf"),
+    caption : [Axuste no caso de fluxo parcial.]
+) <fig:axuste-parcial>
+
+#{
+    set align(center)
+    set text(size:0.9em)
+    table(
+        columns: 4,
+        stroke: none,
+        table.hline(),
+        table.header([Modelo], [a], [b], [m]),
+        table.hline(stroke: 0.6pt),
+        [Equivalente], valor("a_fluxo_nulo_eq"), valor("b_fluxo_nulo_eq"), valor("m_fluxo_nulo_eq"),
+        [Independente], valor("a_fluxo_nulo_in"), valor("b_fluxo_nulo_in"), valor("m_fluxo_nulo_in"),
+        table.hline(),
+    )
+}
+
+#figure(
+    image("/figuras/axuste_fluxo_nulo.pdf"),
+    caption : [Axuste no caso de fluxo nulo.]
+) <fig:axuste-nulo>
